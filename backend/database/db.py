4c3d1625b36d5bc2ca60e027fa12908dd98cdfca
sureshraj-21 <sureshraj2170v@gmail.com>
2026-04-06 17:34:04 +0530

    Hybrid DB config

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL 
-
+engine = create_engine(DATABASE_URL)
 # 🛡️ Step 1: URL Prefix Fix
 # Render URLs often start with 'postgres://', SQLAlchemy needs 'postgresql://'
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
